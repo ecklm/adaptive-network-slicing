9958c57d7579ea94102911979bc7dd2c138cf38b
Máté Eckl <ecklm94@gmail.com>
2019-12-24 19:01:03 +0100

    controller: Set queue explicitly for udp_dst 5001
    This makes it appear explicitly into the flow table therefore statistics
    are also made for this flow.

--- slicing/controller/set-qos.sh
+++ slicing/controller/set-qos.sh
@@ -15,6 +15,9 @@ curl -X POST -d '{"port_name": "s1-eth1", "type": "linux-htb", "max_rate": "5000
 
 # Set the differentiated flow to the differentiated queue
 curl -X POST -d '{"match": {"nw_dst": "10.0.0.1", "nw_proto": "UDP", "tp_dst":
+	  "5001"}, "actions":{"queue": "0"}}' \
+	http://localhost:8080/qos/rules/0000000000000001 | jq .
+curl -X POST -d '{"match": {"nw_dst": "10.0.0.1", "nw_proto": "UDP", "tp_dst":
 	  "5002"}, "actions":{"queue": "1"}}' \
 	http://localhost:8080/qos/rules/0000000000000001 | jq .
 curl -X POST -d '{"match": {"nw_dst": "10.0.0.1", "nw_proto": "UDP", "tp_dst":
