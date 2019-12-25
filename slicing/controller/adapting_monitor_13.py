8e2c2b156d5c9ef24dcd327c884a799bbc9d05fe
Máté Eckl <ecklm94@gmail.com>
2019-12-25 20:10:23 +0100

    controller: Implement basic measurement
    This patch adds a window-based measurement to the controller.
    
    IMPORTANT: The monitoring gets the number of MATCHED bytes per flow,
    not the number which have actually been transmitted in the end.
    
    Now it provides the following output for clarifying the numbers and
    functionality:
    
        avg (B): 1931160.0
                avg_speed (B/s): 386232.0
                avg_speed (b/s): 3089856.0
                avg_speed (Kb/s): 3089.856
                avg_speed (Mb/s): 3.089856
    
        avg (B): 18345432.0
                avg_speed (B/s): 3669086.4
                avg_speed (b/s): 29352691.2
                avg_speed (Kb/s): 29352.691199999997
                avg_speed (Mb/s): 29.3526912
    
        avg (B): 18207672.0
                avg_speed (B/s): 3641534.4
                avg_speed (b/s): 29132275.2
                avg_speed (Kb/s): 29132.2752
                avg_speed (Mb/s): 29.1322752
    
        datapath         ipv4-dst   udp-dst queue-id packets  bytes
        ---------------- ---------- ------- -------- -------- -----------
        0000000000000001   10.0.0.1    5001        0   631851   955358712
        0000000000000001   10.0.0.1    5002        1   735244  1111688928
        0000000000000001   10.0.0.1    5003        2   720561  1089488232

--- slicing/controller/adapting_monitor_13.py
+++ slicing/controller/adapting_monitor_13.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Dict, Any
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.ofproto import ofproto_v1_3
@@ -10,11 +12,13 @@ import json
 
 class AdaptingMonitor13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    time_step = 5  # The number of seconds between two stat request
 
     def __init__(self, *args, **kwargs):
         super(AdaptingMonitor13, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.qos_managers = {}
+        self.stats = FlowStatManager(AdaptingMonitor13.time_step)
         self.monitor_thread = hub.spawn(self._monitor)
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
@@ -36,7 +40,7 @@ class AdaptingMonitor13(app_manager.RyuApp):
         while True:
             for dp in self.datapaths.values():
                 self._request_stats(dp)
-            hub.sleep(5)
+            hub.sleep(AdaptingMonitor13.time_step)
 
     def _request_stats(self, datapath):
         self.logger.debug('send stats request: %016x', datapath.id)
@@ -69,6 +73,27 @@ class AdaptingMonitor13(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
+        flowstats = sorted([flow for flow in body if flow.priority == 1 and flow.table_id == 0],
+                           key=lambda flow: (flow.match['ipv4_dst'], flow.match['udp_dst']))
+        for stat in flowstats:
+            # WARNING: stat.byte_count is the number of bytes that MATCHED the rule, not the number of bytes
+            # that have finally been transmitted. This is not a problem for us, but it is important to know
+            self.stats.put(ev.msg.datapath.id, stat.match['ipv4_dst'], stat.match['udp_dst'], stat.byte_count)
+
+            # Log the stats
+            self.logger.info("")
+            avg = self.stats.get_avg(ev.msg.datapath.id, stat.match['ipv4_dst'], stat.match['udp_dst'])
+            avg_speed = self.stats.get_avg_speed(ev.msg.datapath.id, stat.match['ipv4_dst'], stat.match['udp_dst'])
+            self.logger.info(
+                "avg (B): {}\n\tavg_speed (B/s): {}\n\tavg_speed (b/s): {}\n\t"
+                "avg_speed (Kb/s): {}\n\tavg_speed (Mb/s): {}".format(
+                    avg,
+                    avg_speed,
+                    avg_speed * 8,
+                    avg_speed * 8 / 1000,
+                    avg_speed * 8 / 1000000
+                )
+            )
 
 
 class QoSManager:
@@ -144,3 +169,89 @@ class QoSManager:
                                  json.dumps(r.json(), indent=4, sort_keys=True)))
         except ValueError:
             log("{} - {}".format(r.status_code, r.text))
+
+
+class FlowStat:
+    window_size = 10  # The number of data stored for statistical calculations
+    scaling_prefixes = {'K': 1 / 1000, 'M': 1 / 1000000, 'G': 1 / 1000000000, None: 1}
+
+    def __init__(self, time_step: int):
+        """
+        :param time_step: The number of seconds between two measurements.
+        """
+        self.data = []
+        self.time_step = time_step
+
+    def put(self, val: int):
+        if len(self.data) < FlowStat.window_size:
+            self.data.append(val)
+        else:
+            self.data = self.data[1:] + [val]
+
+    def get_avg(self, prefix: str = None) -> float:
+        """
+        :param prefix: A prefix to scale the result with. See possible values in `FlowStat.scaling_prefixes`
+        :return: The average number of bytes transmitted during the last `window_size` number of measurements
+        """
+        try:
+            return (self.data[-1] - self.data[0]) * FlowStat.scaling_prefixes[prefix] / float(len(self.data) - 1)
+        except ZeroDivisionError:
+            # Just in case accidentally called on an empty dataset
+            return 0.0
+
+    def get_avg_speed(self, prefix: str = None) -> float:
+        """
+        :param prefix: See `FlowStat.get_avg` parameter documentation
+        :return: The average throughput of the Flow during the last `window_size` number of measurements in **Bytes/s**
+        """
+        return self.get_avg(prefix) / float(self.time_step)
+
+    def get_avg_speed_bps(self, prefix: str = None) -> float:
+        """
+        :param prefix: See `FlowStat.get_avg` parameter documentation
+        :return: The average throughput of the Flow during the last `window_size` number of measurements in **bits/s**
+        """
+        return self.get_avg_speed(prefix) * 8
+
+
+class FlowStatManager:
+    def __init__(self, time_step):
+        self.stats: Dict[Tuple[int, str, int], FlowStat] = {}
+        self.time_step = time_step
+
+    def put(self, dpid: int, ipv4_dst: str, udp_dst: int, val: int) -> None:
+        """
+        Adds a new record to the specified flow's stats
+
+        :param dpid: Datapath ID
+        :param ipv4_dst: IPv4 destination address
+        :param udp_dst: UDP destination port
+        :param val: The measurement value
+        """
+        key = (dpid, ipv4_dst, udp_dst)
+        try:
+            self.stats[key].put(val)
+        except KeyError:
+            self.stats[key] = FlowStat(self.time_step)
+            self.stats[key].put(val)
+
+    def get_avg(self, dpid: int, ipv4_dst: str, upd_dst: int, prefix: str = None) -> float:
+        """
+        :param prefix: See `FlowStat.get_avg` parameter documentation
+        :return: The result of `FlowStat.get_avg` for the given flow
+        """
+        return self.stats[(dpid, ipv4_dst, upd_dst)].get_avg(prefix)  # Let the KeyError exception arise if any
+
+    def get_avg_speed(self, dpid: int, ipv4_dst: str, upd_dst: int, prefix: str = None) -> float:
+        """
+        :param prefix: See `FlowStat.get_avg` parameter documentation
+        :return: The result of `FlowStat.get_avg_speed` for the given flow
+        """
+        return self.stats[(dpid, ipv4_dst, upd_dst)].get_avg_speed(prefix)
+
+    def get_avg_speed_bps(self, dpid: int, ipv4_dst: str, upd_dst: int, prefix: str = None) -> float:
+        """
+        :param prefix: See `FlowStat.get_avg` parameter documentation
+        :return: The result of `FlowStat.get_avg_bps` for the given flow
+        """
+        return self.stats[(dpid, ipv4_dst, upd_dst)].get_avg_speed_bps(prefix)
