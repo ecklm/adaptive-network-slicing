b61f3d3968ff85059b0ce9310264da4e8ad4e021
Máté Eckl <ecklm94@gmail.com>
2020-03-10 12:56:46 +0100

    mininet: Generalize setup script
    As now the controller is not so specific to the test topology and the
    names, simple topologies do not need to be defined in topology files any
    more.

--- mininet/setup-mininet.sh
+++ mininet/setup-mininet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo mn --custom topologies/simplest.py --topo simplest_topo \
-	--mac \
-	--controller remote,ip=192.0.2.1,port=6653
+default_topo="--topo linear,1,2"
+
+sudo mn --mac --switch ovsk,protocols=OpenFlow13 \
+	--controller remote,ip=192.0.2.1,port=6653 \
+	${@:-$default_topo}
