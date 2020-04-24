6e88026177ce34b98904800b8fcc78983345a92f
Máté Eckl <ecklm94@gmail.com>
2020-04-24 08:56:44 +0200

    controller: Enable passing parameters to the runner script

--- controller/run-controller.sh
+++ controller/run-controller.sh
@@ -4,7 +4,7 @@ source .venv/bin/activate
 
 set -v
 
-ryu-manager ryu.app.rest_qos \
+ryu-manager $@ ryu.app.rest_qos \
 	ryu.app.rest_conf_switch \
 	qos_simple_switch_13.py \
 	adapting_monitor_13.py
