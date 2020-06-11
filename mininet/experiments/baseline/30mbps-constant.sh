53a131bde2e1c469503a8ea0ca0e55cf52053f7f
Máté Eckl <ecklm94@gmail.com>
2020-06-11 08:23:12 +0200

    mininet: Add experiment script for baseline experiment
    Related: #38

---
+++ mininet/experiments/baseline/30mbps-constant.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source `dirname $0`/../common.sh
+
+dst=$1  # Textual name of the destination host
+case $dst in
+	ue1)
+		IP=$UE1_IP
+		PORT=$UE1_PORT
+		;;
+	ue2)
+		IP=$UE2_IP
+		PORT=$UE2_PORT
+		;;
+	ue3)
+		IP=$UE3_IP
+		PORT=$UE3_PORT
+		;;
+	*)
+		echo "Please specify destination host (ue1, ue2, ue3)" >&2
+		exit 1
+esac
+
+iperf_cmd -c $IP -b 30M -t $EXPERIMENT_LENGTH -p $PORT
