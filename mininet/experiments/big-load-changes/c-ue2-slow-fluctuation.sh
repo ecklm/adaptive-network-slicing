c7f40b9be57962d7050f54bdfe673aff4a41a004
Máté Eckl <ecklm94@gmail.com>
2020-06-12 07:33:34 +0200

    mininet: Change default client bandwidth to 40Mbps
    30Mbps did not demonstrate the difference between the baseline and the
    improvements well enough.
    
    Related: #40

--- mininet/experiments/big-load-changes/c-ue2-slow-fluctuation.sh
+++ mininet/experiments/big-load-changes/c-ue2-slow-fluctuation.sh
@@ -2,7 +2,7 @@
 
 source `dirname $0`/../common.sh
 
-for bw in 30 12 5 17 30 2 7
+for bw in 40 21 5 17 30 2 7
 do
 	title "${bw}Mbps for 90 seconds"
 	iperf_cmd -c $UE2_IP -b ${bw}M -t 90 -p $UE2_PORT
